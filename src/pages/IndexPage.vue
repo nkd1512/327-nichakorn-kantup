cf6204c459f7cb8a189467d1f0466dccdcb85b92
Nichakorn Kantup <nichakornkantup@MacBook-Air-khxng-Nichakorn.local>
2025-10-08 09:37:49 +0700

    Added basic form to IndexPage.vue

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,82 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="row items-center justify-center">
+    <div class="q-pa-md" style="max-width: 400px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+
+        <q-input
+          filled
+          v-model="name"
+          label="Your name *"
+          hint="Name and surname"
+          lazy-rules
+          :rules="[val => (val && val.length > 0) || 'Please type something']"
+        />
+
+        <q-input
+          filled
+          type="number"
+          v-model="age"
+          label="Your age *"
+          lazy-rules
+          :rules="[
+            val => (val !== null && val !== '') || 'Please type your age',
+            val => (val > 0 && val < 100) || 'Please type a real age'
+          ]"
+        />
+
+        <q-toggle v-model="accept" label="I accept the license and terms" />
+
+        <div>
+          <q-btn label="Submit" type="submit" color="primary" :disable="!accept" />
+
+          <q-btn
+            label="Reset"
+            type="reset"
+            color="primary"
+            flat
+            class="q-ml-sm"
+          />
+        </div>
+      </q-form>
+    </div>
   </q-page>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
+import { useQuasar } from 'quasar';
 
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-]);
+const $q = useQuasar();
 
-const meta = ref<Meta>({
-  totalCount: 1200,
-});
+// ----------------- Data Properties -----------------
+const name = ref(null);
+const age = ref(null);
+const accept = ref(false);
+
+// ----------------- Form Methods -----------------
+
+function onSubmit() {
+  if (accept.value !== true) {
+    $q.notify({
+      color: 'red-5',
+      textColor: 'white',
+      icon: 'warning',
+      message: 'You need to accept the license and terms first'
+    });
+  } else {
+    $q.notify({
+      color: 'green-4',
+      textColor: 'white',
+      icon: 'cloud_done',
+      message: 'Submitted successfully!'
+    });
+    onReset(); // ล้างฟอร์มหลังจากส่งสำเร็จ
+  }
+}
+
+function onReset() {
+  name.value = null;
+  age.value = null;
+  accept.value = false;
+}
 </script>
